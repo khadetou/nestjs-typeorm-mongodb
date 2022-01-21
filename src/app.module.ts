78c911e306fb2e7ad2970f9d1c611cfa5f5bb798
khadetou <khadetou96@gmail.com>
2022-01-21 18:17:50 +0000

    profile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { configValidationSchema } from './config.schema';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { configValidationSchema } from './config.schema';
       },
     }),
     AuthModule,
+    ProfileModule,
   ],
 })
 export class AppModule {}
