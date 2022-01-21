7494006efb8d1c08b5f4e11492082a93f638f0c0
khadetou <khadetou96@gmail.com>
2022-01-21 13:58:04 +0000

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`, '.env'],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mongodb',
+          url: configService.get('MONGO_URI'),
+          useNewUrlParser: true,
+          synchronize: true,
+          useUnifiedTopology: true,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
