5c28a8005b09d41b67d6848c1dd8562a9ff555cd
khadetou <khadetou96@gmail.com>
2022-01-21 12:22:52 +0000

    auth credential

---
+++ src/auth/dto/auth-crendentials.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/,
+  )
+  password: string;
+}
