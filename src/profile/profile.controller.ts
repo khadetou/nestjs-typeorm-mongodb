f193a86160fc428b053a894b7806c53650eea06d
khadetou <khadetou96@gmail.com>
2022-01-22 11:42:31 +0000

    create profile service && controller

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { User } from './../auth/user.entity';
+import { CreateProfileDto, SocialsDto } from './dto/create-profile.dto';
+import { ProfileService } from './profile.service';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { GetUser } from 'src/auth/get-user.decorator';
+import { Profile } from './profile.entity';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+  constructor(private profileService: ProfileService) {}
+
+  //CREATE PROFILE
+  @Post()
+  createProfile(
+    @Body() createProfileDto: CreateProfileDto,
+    @Body() socialsDto: SocialsDto,
+    @GetUser() user: User,
+  ): Promise<Profile> {
+    return this.profileService.createProfile(
+      createProfileDto,
+      socialsDto,
+      user,
+    );
+  }
+}
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,23 @@
+import { User } from './../auth/user.entity';
+import { CreateProfileDto, SocialsDto } from './dto/create-profile.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { ProfileRepository } from './profile.repository';
+import { Profile } from './profile.entity';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectRepository(ProfileRepository)
+    private profileRepository: ProfileRepository,
+  ) {}
+
+  //CREATE PROFILE
+  createProfile(
+    createProfileDto: CreateProfileDto,
+    socialsDto: SocialsDto,
+    user: User,
+  ): Promise<Profile> {
+    return this.createProfile(createProfileDto, socialsDto, user);
+  }
+}
